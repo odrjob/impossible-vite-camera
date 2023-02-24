4016bf242b72f36e925e3d570c85a10de8043c51
julieturin <j.turinlol@hotmail.fr>
2023-02-24 11:46:50 +0100

    salut

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,23 +1,24 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+document.addEventListener("DOMContentLoaded", () => {
+  var but = document.getElementById("but");
+  var video = document.getElementById("vid");
+  var mediaDevices = navigator.mediaDevices;
+  vid.muted = true;
+  but.addEventListener("click", () => {
+  
+    // Accessing the user camera and video.
+    mediaDevices
+      .getUserMedia({
+        video: true,
+        audio: true,
+      })
+      .then((stream) => {
+  
+        // Changing the source of video to current stream.
+        video.srcObject = stream;
+        video.addEventListener("loadedmetadata", () => {
+          video.play();
+        });
+      })
+      .catch(alert);
+  });
+});
